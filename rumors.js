4017b9b51bf7599350652bc03f53b9f4cb1faf30
antocs <roxasthethund@yahoo.it>
2020-10-28 09:37:00 +0100

    Update 2.2.0 datamine (rumors.js)

--- rumors.js
+++ rumors.js
@@ -1,8 +1,8 @@
-export const rumors = [
-  {
+export const rumors = [{
     name: ["Story Event: Fight or Fright",
-          "Acerola & Mimikyu Seasonal Scout [with grid and EX]",
-          "Hilbert & Mightyena Seasonal Scout [with grid and EX]"],
+      "Acerola & Mimikyu Seasonal Scout [with grid and EX]",
+      "Hilbert & Mightyena Seasonal Scout [with grid and EX]"
+    ],
     date: "October 15, 2020",
     expires: "November 2, 2020"
   },
@@ -32,12 +32,79 @@ export const rumors = [
     expires: "November 11, 2020"
   },
   {
+    name: ["Lisia & Altaria Spotlight Scout [with grid and EX",
+      "Story Event: Dazzling Dizzying Battle Stage"
+    ],
+    date: "October 30, 2020",
+    expires: "November 12, 2020"
+  },
+  {
+    name: ["Olivia & Midnight Lycanroc [with grid] and Kukui & Midday Lycanroc Spotlight Scout"],
+    date: "November 1, 2020",
+    expires: "November 11, 2020"
+  },
+  {
+    name: ["Legendary Event: The Dragon That Rules the Sky"],
+    date: "November 1, 2020",
+    expires: "December 7, 2020"
+  },
+  {
+    name: ["Plumeria & Salazzle Spotlight Scout",
+      "Psychic- and Poison-Type Egg Event"
+    ],
+    date: "November 5, 2020",
+    expires: "November 19, 2020"
+  },
+  {
+    name: ["Blue & Pidgeot Spotlight Scout [with grid and EX",
+      "Legendary Arena: Tapu Bulu"
+    ],
+    date: "November 11, 2020",
+    expires: "November 25, 2020"
+  },
+  {
+    name: ["Nate & Braviary Spotlight Scout [with grid and EX",
+      "Time Trial Event: Race to Victory"
+    ],
+    date: "November 12, 2020",
+    expires: "November 30, 2020"
+  },
+  {
+    name: ["Bianca & Musharna Spotlight Scout [with grid and EX"],
+    date: "November 14, 2020",
+    expires: "November 30, 2020"
+  },
+  {
+    name: ["Rosa & Delibird (Holiday 2019) [with grid and EX] and Siebold & Octillery (Holiday 2019) [with grid]"],
+    date: "November 16, 2020",
+    expires: "November 30, 2020"
+  },
+  {
     name: ["Battle Villa"],
     date: "November 19, 2020",
     expires: "December 4, 2020"
   },
   {
-    name: ["Sygna Suit Blue & Blastoise [with grid & EX]",],
+    name: ["New Story Chapters!"],
+    date: "November 19, 2020"
+  },
+  {
+    name: ["Cynthia & Garchomp and Steven & Metagross Pokéfair Scout [both with EX]"],
+    date: "November 23, 2020",
+    expires: "December 14, 2020"
+  },
+  {
+    name: ["5★-Select Special Scout ×11"],
+    date: "November 23, 2020",
+    expires: "December 8, 2020"
+  },
+  {
+    name: ["Blissful Bonanza"],
+    date: "November 13, 2020",
+    expires: "December 1, 2020"
+  },
+  {
+    name: ["Sygna Suit Blue & Blastoise [with grid & EX]", ],
     date: "August 28, 2020",
     expires: "October 30, 2020"
   },
