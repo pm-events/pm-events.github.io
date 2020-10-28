ab4f4239e765035a7336133092e9712c206b617f
antocs <roxasthethund@yahoo.it>
2020-10-28 12:46:24 +0100

    Fix replace error

--- client.js
+++ client.js
@@ -20,7 +20,7 @@ document.getElementById("rumors").onclick = () => {
     const events = rumors.filter(e => new Date(e.date + " 06:00:00 UTC").getTime() - Date.now() >= 0 || e.date === "TBA");
     const nextEvents = events.filter(e => e.date === events[0].date);
     const futureEvents = events.filter(e => e.date !== events[0].date);
-    document.getElementById("rumorstitles").innerHTML = nextEvents.length >= 1 ? "<b style=\"\">" + nextEvents.map(e => e.name.replace("[grid]", "<img src=\"./grid.png\"/>").replace("[ex]", "<img src=\"./ex.png\"/>").replace("[gridEX]", "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>")).flat().join("<br>") + "</b>" : "No rumors yet.";
+    document.getElementById("rumorstitles").innerHTML = nextEvents.length >= 1 ? "<b style=\"\">" + nextEvents.map(e => e.name.map(a => a.replace("[grid]", "<img src=\"./grid.png\"/>").replace("[ex]", "<img src=\"./ex.png\"/>").replace("[gridEX]", "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>"))).flat().join("<br>") + "</b>" : "No rumors yet.";
     document.getElementById("rumorsdate").innerHTML = nextEvents.length >= 1 ? nextEvents[0].date !== "TBA" ? "<span style=\"font-size:10px\"><i>" + new Date(nextEvents[0].date + " 06:00:00 UTC").toDateString() + "</i></span>" : null : null;
     const countdown = events.length >= 1 ? new Date(events[0].date + " 06:00:00 UTC").getTime() - Date.now() : null;
     const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
@@ -28,7 +28,7 @@ document.getElementById("rumors").onclick = () => {
     const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
     document.getElementById("rumorscountdown").innerHTML = nextEvents.length > 0 ? nextEvents[0].date !== "TBA" ? days >= 1 ? `${days}d ${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : hours >= 1 ? `${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : minutes >=1 ? `${minutes}m <span style="color:#ffc361">${seconds}</span>s` : `<span style="color:#ffc361">${seconds}</span>s` : "TBA" : null;
-    document.getElementById("futurerumors").innerHTML = nextEvents.length >= 1 ? futureEvents.map(e => e.name.map(n => `${n} - <span style="color:#ffc361;">${new Date(e.date + " 06:00:00 UTC").toDateString()}</span>`)).flat().join("<br>") : null;
+    document.getElementById("futurerumors").innerHTML = nextEvents.length >= 1 ? futureEvents.map(e => e.name.map(n => `${n} - <span style="color:#ffc361;">${new Date(e.date + " 06:00:00 UTC").toDateString()}</span>`.replace("[grid]", "<img src=\"./grid.png\"/>").replace("[ex]", "<img src=\"./ex.png\"/>").replace("[gridEX]", "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>"))).flat().join("<br>") : null;
   }, 1000);
   document.getElementById("rumorsdiv").style.display = "inline-block";
 }
@@ -66,7 +66,7 @@ document.getElementById("ongoing").onclick = () => {
   document.getElementById("ongoing").className = "active";
   setInterval(() => {
     const events = rumors.filter(e => new Date(e.date + " 06:00:00 UTC").getTime() - Date.now() <= 0 && new Date(e.expires + " 06:00:00 UTC").getTime() >= Date.now());
-    document.getElementById("ongoinglist").innerHTML = events.sort((a, b) => new Date(a.expires + " 06:00:00 UTC").getTime() - new Date(b.expires + " 06:00:00 UTC").getTime()).map(e => e.name.replace("[grid]", "<img src=\"./grid.png\"/>").replace("[ex]", "<img src=\"./ex.png\"/>").replace("[gridEX]", "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>").map(n => `${n} - Ends on <span style="color:#ffc361;">${new Date(e.expires + " 06:00:00 UTC").toDateString()}</span>`)).flat().join("<br>") || "No events running at the moment.";
+    document.getElementById("ongoinglist").innerHTML = events.sort((a, b) => new Date(a.expires + " 06:00:00 UTC").getTime() - new Date(b.expires + " 06:00:00 UTC").getTime()).map(e => e.name.map(n => `${n} - Ends on <span style="color:#ffc361;">${new Date(e.expires + " 06:00:00 UTC").toDateString()}</span>`.replace("[grid]", "<img src=\"./grid.png\"/>").replace("[ex]", "<img src=\"./ex.png\"/>").replace("[gridEX]", "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>"))).flat().join("<br>") || "No events running at the moment.";
   }, 1000);
   document.getElementById("ongoingdiv").style.display = "inline-block";
 }
