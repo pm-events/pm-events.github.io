055ab5c9d576a3a451e2d869d3a332e20fbfa0ee
antocs <roxasthethund@yahoo.it>
2020-10-31 11:57:17 +0100

    Fix again

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
-import { rumors } from "./rumors.js"
-import { announced } from "./announced.js"
+import {
+  rumors
+} from "./rumors.js"
+import {
+  announced
+} from "./announced.js"
 let active;
 
 
@@ -10,9 +14,9 @@ document.getElementById("twitteren").onclick = () => window.open("https://twitte
 document.getElementById("twitterjp").onclick = () => window.open("https://twitter.com/pokemas_game");
 
 document.getElementById("rumors").onclick = () => {
-  if(active) {
+  if (active) {
     document.getElementById(active).className = "";
-    document.getElementById(active+"div").style.display = "none";
+    document.getElementById(active + "div").style.display = "none";
   }
   active = "rumors";
   document.getElementById("rumors").className = "active";
@@ -20,23 +24,23 @@ document.getElementById("rumors").onclick = () => {
     const events = rumors.filter(e => new Date(e.date + " 06:00:00 UTC").getTime() - Date.now() >= 0 || e.date === "TBA");
     const nextEvents = events.filter(e => e.date === events[0].date);
     const futureEvents = events.filter(e => e.date !== events[0].date);
-    document.getElementById("rumorstitles").innerHTML = nextEvents.length >= 1 ? "<b style=\"\">" + nextEvents.map(e => e.name.map(a => a.replace("[grid]", "<img src=\"./grid.png\"/>").replace("[ex]", "<img src=\"./ex.png\"/>").replace("[gridEX]", "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>"))).flat().join("<br>") + "</b>" : "No rumors yet.";
+    document.getElementById("rumorstitles").innerHTML = nextEvents.length >= 1 ? "<b style=\"\">" + nextEvents.map(e => e.name.map(a => a.replace(/\[grid\]/g, "<img src=\"./grid.png\"/>").replace(/\[ex\]/g, "<img src=\"./ex.png\"/>").replace(/\[gridEX\]/g, "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>"))).flat().join("<br>") + "</b>" : "No rumors yet.";
     document.getElementById("rumorsdate").innerHTML = nextEvents.length >= 1 ? nextEvents[0].date !== "TBA" ? "<span style=\"font-size:10px\"><i>" + new Date(nextEvents[0].date + " 06:00:00 UTC").toDateString() + "</i></span>" : null : null;
     const countdown = events.length >= 1 ? new Date(events[0].date + " 06:00:00 UTC").getTime() - Date.now() : null;
     const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
     const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
-    document.getElementById("rumorscountdown").innerHTML = nextEvents.length > 0 ? nextEvents[0].date !== "TBA" ? days >= 1 ? `${days}d ${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : hours >= 1 ? `${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : minutes >=1 ? `${minutes}m <span style="color:#ffc361">${seconds}</span>s` : `<span style="color:#ffc361">${seconds}</span>s` : "TBA" : null;
-    document.getElementById("futurerumors").innerHTML = nextEvents.length >= 1 ? futureEvents.map(e => e.name.map(n => `${n} - <span style="color:#ffc361;">${new Date(e.date + " 06:00:00 UTC").toDateString()}</span>`.replace("[grid]", "<img src=\"./grid.png\"/>").replace("[ex]", "<img src=\"./ex.png\"/>").replace("[gridEX]", "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>"))).flat().join("<br>") : null;
+    document.getElementById("rumorscountdown").innerHTML = nextEvents.length > 0 ? nextEvents[0].date !== "TBA" ? days >= 1 ? `${days}d ${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : hours >= 1 ? `${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : minutes >= 1 ? `${minutes}m <span style="color:#ffc361">${seconds}</span>s` : `<span style="color:#ffc361">${seconds}</span>s` : "TBA" : null;
+    document.getElementById("futurerumors").innerHTML = nextEvents.length >= 1 ? futureEvents.map(e => e.name.map(n => `${n} - <span style="color:#ffc361;">${new Date(e.date + " 06:00:00 UTC").toDateString()}</span>`.replace(/\[grid\]/g, "<img src=\"./grid.png\"/>").replace(/\[ex\]/g, "<img src=\"./ex.png\"/>").replace(/\[gridEX\]/g, "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>"))).flat().join("<br>") : null;
   }, 1000);
   document.getElementById("rumorsdiv").style.display = "inline-block";
 }
 
 document.getElementById("official").onclick = () => {
-  if(active) {
+  if (active) {
     document.getElementById(active).className = "";
-    document.getElementById(active+"div").style.display = "none";
+    document.getElementById(active + "div").style.display = "none";
   }
   active = "official";
   document.getElementById("official").className = "active";
@@ -51,16 +55,16 @@ document.getElementById("official").onclick = () => {
     const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
-    document.getElementById("officialcountdown").innerHTML = nextEvents.length > 0 ? nextEvents[0].date !== "TBA" ? days >= 1 ? `${days}d ${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : hours >= 1 ? `${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : minutes >=1 ? `${minutes}m <span style="color:#ffc361">${seconds}</span>s` : `<span style="color:#ffc361">${seconds}</span>s` : "<span style=\"color:#ffc361\">Soon™</span>" : null;
+    document.getElementById("officialcountdown").innerHTML = nextEvents.length > 0 ? nextEvents[0].date !== "TBA" ? days >= 1 ? `${days}d ${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : hours >= 1 ? `${hours}h ${minutes}m <span style="color:#ffc361">${seconds}</span>s` : minutes >= 1 ? `${minutes}m <span style="color:#ffc361">${seconds}</span>s` : `<span style="color:#ffc361">${seconds}</span>s` : "<span style=\"color:#ffc361\">Soon™</span>" : null;
     document.getElementById("futureofficial").innerHTML = nextEvents.length >= 1 ? futureEvents.map(e => e.name.map(n => `${n} - <span style="color:#ffc361;">${e.date !== "TBA" ? new Date(e.date + " 06:00:00 UTC").toDateString() : "Soon™"}</span>`)).flat().join("<br>") : null;
   }, 1000);
   document.getElementById("officialdiv").style.display = "inline-block";
 }
 
 document.getElementById("ongoing").onclick = () => {
-  if(active) {
+  if (active) {
     document.getElementById(active).className = "";
-    document.getElementById(active+"div").style.display = "none";
+    document.getElementById(active + "div").style.display = "none";
   }
   active = "ongoing";
   document.getElementById("ongoing").className = "active";
@@ -72,9 +76,9 @@ document.getElementById("ongoing").onclick = () => {
 }
 
 document.getElementById("utilities").onclick = () => {
-  if(active) {
+  if (active) {
     document.getElementById(active).className = "";
-    document.getElementById(active+"div").style.display = "none";
+    document.getElementById(active + "div").style.display = "none";
   }
   active = "utilities";
   document.getElementById("utilities").className = "active";
