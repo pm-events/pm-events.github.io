61504e6bd454b5bfde7b63943666bb4a6a715764
antocs <roxasthethund@yahoo.it>
2020-12-06 11:32:30 +0100

    fix

--- announced.js
+++ announced.js
@@ -1,11 +1,6 @@
 export const announced = [{
-    name: ["Gloria & Zacian"],
-    date: "November 30, 2020"
-  },
-  {
-    name: ["Skyla & Togekiss [Christmas 2020]",
-      "Erika & Comfey [Christmas 2020]"
-    ],
-    date: "December 10, 2020"
-  }
-];
+  name: ["Skyla & Togekiss [Christmas 2020]",
+    "Erika & Comfey [Christmas 2020]"
+  ],
+  date: "December 10, 2020"
+}];
--- rumors.js
+++ rumors.js
@@ -4,33 +4,6 @@ export const rumors = [{
     expires: "December 7, 2020"
   },
   {
-    name: ["Alder & Volcarona Pokéfair Scout[gridEX]"],
-    date: "November 6, 2020",
-    expires: "November 30, 2020"
-  },
-  {
-    name: ["Nate & Braviary[gridEX] Spotlight Scout",
-      "Time Trial Event: Race to Victory"
-    ],
-    date: "November 12, 2020",
-    expires: "November 30, 2020"
-  },
-  {
-    name: ["Bianca & Musharna[gridEX] Spotlight Scout"],
-    date: "November 14, 2020",
-    expires: "November 30, 2020"
-  },
-  {
-    name: ["Rosa & Delibird (Holiday 2019)[gridEX] and Siebold & Octillery (Holiday 2019)[grid] Seasonal Scout"],
-    date: "November 16, 2020",
-    expires: "November 30, 2020"
-  },
-  {
-    name: ["Battle Villa"],
-    date: "November 19, 2020",
-    expires: "December 4, 2020"
-  },
-  {
     name: ["Cynthia & Garchomp[ex] and Steven & Metagross Pokéfair Scout[ex]"],
     date: "November 23, 2020",
     expires: "December 14, 2020"
@@ -39,11 +12,6 @@ export const rumors = [{
     name: ["5★-Select Special Scout ×11"],
     date: "November 23, 2020",
     expires: "December 8, 2020"
-  },
-  {
-    name: ["Blissful Bonanza"],
-    date: "November 23, 2020",
-    expires: "December 1, 2020"
   },
   {
     name: ["Prestige Event"],
@@ -73,7 +41,7 @@ export const rumors = [{
     expires: "December 21, 2020"
   },
   {
-    name: ["Legendary Event: The Best That Devours The Sun [Solgaleo]"],
+    name: ["Legendary Event: The Beast That Devours The Sun [Solgaleo]"],
     date: "December 7, 2020",
     expires: "January 4, 2021"
   },
