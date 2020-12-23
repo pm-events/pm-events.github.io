8724090c09afc029f61484718986146504f07197
jackylau5 <47999799+jackylau5@users.noreply.github.com>
2020-12-23 12:51:00 -0500

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,76 +1,116 @@
 export const rumors = [{
-    name: ["Legendary Event: The Dragon That Rules the Sky"],
-    date: "November 1, 2020",
-    expires: "December 7, 2020"
+    name: ["Story Event: The Ideal Formula"],
+    date: "December 24, 2020",
+    expires: "January 12, 2021"
   },
   {
-    name: ["Cynthia & Garchomp[ex] and Steven & Metagross Pokéfair Scout[ex]"],
-    date: "November 23, 2020",
-    expires: "December 14, 2020"
+    name: ["N [gridEX] Poké Fair Scout"],
+    date: "December 24, 2020",
+    expires: "January 29, 2021"
   },
   {
-    name: ["5★-Select Special Scout ×11"],
-    date: "November 23, 2020",
-    expires: "December 8, 2020"
-  },
-  {
-    name: ["Prestige Event"],
-    date: "November 30, 2020",
-    expires: "December 7, 2020"
-  },
-  {
-    name: ["Gloria & Zacian[gridEX] Pokéfair Scout"],
-    date: "November 30, 2020",
-    expires: "December 12, 2020"
-  },
-  {
-    name: ["Battle Buffet Bash"],
-    date: "November 30, 2020",
-    expires: "December 24, 2020"
-  },
-  {
-    name: ["Jasmine & Steelix[ex] Spotlight Scout"],
-    date: "December 2, 2020",
-    expires: "December 21, 2020"
-  },
-  {
-    name: ["Legendary Arena - Regirock",
-      "Dawn & Turtwig[ex] and Brendan & Treecko[gridEX] Spotlight Scout"
-    ],
-    date: "December 7, 2020",
-    expires: "December 21, 2020"
-  },
-  {
-    name: ["Legendary Event: The Beast That Devours The Sun [Solgaleo]"],
-    date: "December 7, 2020",
-    expires: "January 4, 2021"
-  },
-  {
-    name: ["Deck the Halls",
-      "Skyla & Togekiss [Holiday 2020][gridEX] Seasonal Scout",
-      "Erika & Comfey [Holiday 2020][gridEX] Seasonal Scout"
-    ],
-    date: "December 10, 2020",
+    name: ["Kanto and Johto 5★-Guaranteed Scout"],
+    date: "December 28, 2020",
     expires: "January 1, 2021"
   },
   {
-    name: ["Battle Villa Challenge"],
-    date: "December 14, 2020",
-    expires: "December 29, 2020"
-  },
-  {
-    name: ["Fire-, Rock-, and Bug-Type Egg Event"],
-    date: "December 17, 2020",
-    expires: "December 31, 2020"
-  },
-  {
-    name: ["Triple Feature Pokéfair Scout"],
-    date: "December 17, 2020",
+    name: ["Hoenn and Sinnoh 5★-Guaranteed Scout"],
+    date: "December 29, 2020",
     expires: "January 1, 2021"
   },
   {
-    name: ["Special Event: Blasting Off Again! [Free James & Weezing]"],
-    date: "September 17, 2020",
-    expires: "January 25, 2021"
+    name: ["Unova 5★-Guaranteed Scout"],
+    date: "December 30, 2020",
+    expires: "January 1, 2021"
+  },
+  {
+    name: ["Kalos and Alola 5★-Guaranteed Scout"],
+    date: "December 31, 2020",
+    expires: "January 1, 2021"
+  },
+  {
+    name: ["5★-Select Special Scout ×15"],
+    date: "January 1, 2021",
+    expires: "January 14, 2021"
+  },
+  {
+    name: ["5★-Select Special Scout ×25"],
+    date: "January 1, 2021",
+    expires: "January 14, 2021"
+  },
+  {
+    name: ["Story Event: Season’s Greetings"],
+    date: "January 1, 2021",
+    expires: "January 14, 2021"
+  },
+  {
+    name: ["Lillie (New Years 2021) [gridEX] Scout + Tiered Scout"],
+    date: "January 1, 2021",
+    expires: "January 14, 2021"
+  },
+  {
+    name: ["Lance (New Years 2021) [gridEX] Scout + Tiered Scout"],
+    date: "January 1, 2021",
+    expires: "January 14, 2021"
+  },
+  {
+    name: ["Legendary Event: Pure Hearts and Rainbow Wings"],
+    date: "January 4, 2021",
+    expires: "February 8, 2021"
+  },
+  {
+    name: ["Electric- and Steel-Type Egg Event"],
+    date: "January 7, 2021",
+    expires: "January 21, 2021"
+  },
+  {
+    name: ["Battle Villa"],
+    date: "January 7, 2021",
+    expires: "January 21, 2021"
+  },
+  {
+    name: ["Sygna Suit Elesa & Volkner Double Feature Spotlight Scout"],
+    date: "January 7, 2021",
+    expires: "January 21, 2021"
+  },
+  {
+    name: ["6★ [EX] Skyla Sync Pair Support Rally"],
+    date: "January 11, 2021",
+    expires: "February 18, 2021"
+  },
+  {
+    name: ["Solo Event: Team Hoenn Unite!"],
+    date: "January 14, 2021",
+    expires: "February 1, 2021"
+  },
+  {
+    name: ["May [gridEX] Spotlight Scout"],
+    date: "January 14, 2021",
+    expires: "February 1, 2021"
+  },
+  {
+    name: ["Lisia, Wally, and Phoebe Spotlight Scout"],
+    date: "January 14, 2021",
+    expires: "February 1, 2021"
+  },
+  {
+    name: ["Wallace and Sygna Suit Grimsley Spotlight Scout"],
+    date: "January 18, 2021",
+    expires: "January 28, 2021"
+  },
+  {
+    name: ["Mallow [gridEX] Spotlight Scout"],
+    date: "January 21, 2021",
+    expires: "February 4, 2021"
+  },
+  {
+    name: ["Blissful Bonanza"],
+    date: "January 21, 2021",
+    expires: "January 29, 2021"
+  },
+  {
+    name: ["Battle Villa Johto Challenge"],
+    date: "December 28, 2020",
+    expires: "February 1, 2021"
   }
 ];
