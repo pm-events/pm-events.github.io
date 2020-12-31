ac660add2e22e6d8938d82af39729ab0443b49b2
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2020-12-30 20:16:29 -0500

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,7 +1,7 @@
 export const announced = [{
   name: ["Story Event: Season’s Greetings",
-    "Lillie (New Years 2021) [gridEX] Scout + Tiered Scout",
-    "Lance (New Years 2021) [gridEX] Scout + Tiered Scout"
+    "Lillie (New Years 2021) Scout",
+    "Lance (New Years 2021) Scout"
   ],
   date: "January 1, 2021"
 }];
