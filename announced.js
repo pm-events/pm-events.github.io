91a8d4c5a6eb0d7f0c9fd509e8044c929b747617
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-01-17 21:26:35 -0500

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,7 +1,38 @@
-export const announced = [{
-  name: ["Story Event: Season’s Greetings",
-    "Lillie (New Years 2021) Scout",
-    "Lance (New Years 2021) Scout"
-  ],
-  date: "January 1, 2021"
-}];
+export const announced = [
+  {
+    name: ["Wallace [ex] Unlocked"],
+    date: "January 18, 2021"
+  },
+  {
+    name: ["Mallow Spotlight Scout"],
+    date: "January 21, 2021"
+  },
+  {
+    name: ["Blissful Bonanza"],
+    date: "January 21, 2021
+  },
+  {
+    name: ["Valentine's Day Story Event","Serena & Whimsicott (Valentine's Day 2021) Scout"],
+    date: "January 29, 2021
+  },
+  {
+    name: ["Battle Villa Challenge","Fighting-, Grass-, and Rock-Type Egg Event"],
+    date: "February 4, 2021"
+  },
+  {
+    name: ["Legendary Event: Lurking Shadow [Mewtwo]"],
+    date: "February 8, 2021"
+  },
+  {
+    name: ["Legendary Event: Give and Take [Xerneas & Yveltal]"],
+    date: "February 11, 2021"
+  },
+  {
+    name: ["6★ [ex] Skyla Unlocked","6★ [ex] Korrina Unlocked"],
+    date: "TBA"
+  },
+  {
+    name: ["6★ [ex] Barry Unlocked"],
+    date: "TBA"
+  },
+];
