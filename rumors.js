94a81809665632b3bb71a644873221665dffb52d
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-01-26 17:42:34 -0500

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,15 +1,5 @@
 export const rumors = [
   {
-    name: ["Special Event: Blasting Off Again! [Free James & Weezing]"],
-    date: "September 17, 2020",
-    expires: "January 25, 2021"
-  },
-  {
-    name: ["Special Event: Double Trouble [Free Jessie & Arbok]"],
-    date: "December 18, 2020",
-    expires: "January 25, 2021"
-  },
-  {
     name: ["N [gridEX] Poké Fair Scout"],
     date: "December 24, 2020",
     expires: "January 29, 2021"
@@ -23,21 +13,6 @@ export const rumors = [
     name: ["Legendary Event: Pure Hearts and Rainbow Wings [Ho-Oh]"],
     date: "January 4, 2021",
     expires: "February 8, 2021"
-  },
-  {
-    name: ["Electric- and Steel-Type Egg Event"],
-    date: "January 7, 2021",
-    expires: "January 21, 2021"
-  },
-  {
-    name: ["Battle Villa Challenge"],
-    date: "January 7, 2021",
-    expires: "January 21, 2021"
-  },
-  {
-    name: ["Sygna Suit Elesa & Volkner Double Feature Spotlight Scout"],
-    date: "January 7, 2021",
-    expires: "January 21, 2021"
   },
   {
     name: ["6★ [ex] Skyla Sync Pair Support Rally"],
@@ -78,5 +53,60 @@ export const rumors = [
     name: ["Blissful Bonanza"],
     date: "January 21, 2021",
     expires: "January 29, 2021"
+  },
+  {
+    name: ["Story Event: Baking Buddies","Serena (Palentine's 2021) [gridEX] Seasonal Scout","Dawn (Palentine's 2021) [gridEX] Seasonal Scout"],
+    date: "January 29, 2021",
+    expires: "February 19, 2021"
+  },
+  {
+    name: ["Lillie [gridEX] Spotlight Scout"],
+    date: "January 31, 2021",
+    expires: "February 19, 2021"
+  },
+  {
+    name: ["Fighting-, Grass-, and Rock-Type Egg Event","Battle Villa Challenge"],
+    date: "February 4, 2021",
+    expires: "February 18, 2021"
+  },
+  {
+    name: ["Legendary Event: Lurking Shadow [Mewtwo]"],
+    date: "February 8, 2021",
+    expires: "March 11, 2021"
+  },
+  {
+    name: ["Grimsley and Karen Spotlight Scout"],
+    date: "February 8, 2021",
+    expires: "February 26, 2021"
+  },
+  {
+    name: ["Legendary Event: Give and Take [Xerneas]","Lysandre [gridEX] Poké Fair Scout"],
+    date: "February 11, 2021",
+    expires: "March 4, 2021"
+  },
+  {
+    name: ["Serena [gridEX] and Clemont Spotlight Scout"],
+    date: "February 15, 2021",
+    expires: "March 4, 2021"
+  },
+  {
+    name: ["Legendary Arena: Cresselia"],
+    date: "February 18, 2021",
+    expires: "March 4, 2021"
+  },
+  {
+    name: ["The Story Continues!"],
+    date: "February 18, 2021",
+    expires: "February 25, 2021"
+  },
+  {
+    name: ["Piers [gridEX] Spotlight Scout"],
+    date: "February 18, 2021",
+    expires: "March 11, 2021"
+  },
+  {
+    name: ["Lance [gridEX] and Sygna Suit Cynthia [gridEX] Poké Fair Scout"],
+    date: "February 22, 2021",
+    expires: "March 4, 2021"
   }
 ];
