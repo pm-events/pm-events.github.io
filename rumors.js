7885de94b8b98b35fb74e836225eb608c6574b81
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-02-24 03:06:50 -0500

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,80 +1,5 @@
 export const rumors = [
   {
-    name: ["N [gridEX] Poké Fair Scout"],
-    date: "December 24, 2020",
-    expires: "January 29, 2021"
-  },
-  {
-    name: ["Champion Stadium: Johto Challenge"],
-    date: "December 28, 2020",
-    expires: "February 1, 2021"
-  },
-  {
-    name: ["Legendary Event: Pure Hearts and Rainbow Wings [Ho-Oh]"],
-    date: "January 4, 2021",
-    expires: "February 8, 2021"
-  },
-  {
-    name: ["6★ [ex] Skyla Sync Pair Support Rally"],
-    date: "January 11, 2021",
-    expires: "February 18, 2021"
-  },
-  {
-    name: ["Solo Event: Team Hoenn Unite!"],
-    date: "January 14, 2021",
-    expires: "February 1, 2021"
-  },
-  {
-    name: ["May [gridEX] Spotlight Scout"],
-    date: "January 14, 2021",
-    expires: "February 1, 2021"
-  },
-  {
-    name: ["Lisia, Wally, and Phoebe Spotlight Scout"],
-    date: "January 14, 2021",
-    expires: "February 1, 2021"
-  },
-  {
-    name: ["Legendary Arena - Entei"],
-    date: "January 14, 2021",
-    expires: "January 28, 2021"
-  },
-  {
-    name: ["Wallace and Sygna Suit Grimsley Spotlight Scout"],
-    date: "January 18, 2021",
-    expires: "January 28, 2021"
-  },
-  {
-    name: ["Mallow [gridEX] Spotlight Scout"],
-    date: "January 21, 2021",
-    expires: "February 4, 2021"
-  },
-  {
-    name: ["Blissful Bonanza"],
-    date: "January 21, 2021",
-    expires: "January 29, 2021"
-  },
-  {
-    name: ["Story Event: Baking Buddies","Serena (Palentine's 2021) [gridEX] Seasonal Scout"],
-    date: "January 29, 2021",
-    expires: "February 19, 2021"
-  },
-  {
-    name: ["Lillie [gridEX] Spotlight Scout"],
-    date: "January 31, 2021",
-    expires: "February 19, 2021"
-  },
-  {
-    name: ["Dawn (Palentine's 2021) [gridEX] Seasonal Scout"],
-    date: "February 1, 2021",
-    expires: "February 19, 2021"
-  },
-  {
-    name: ["Fighting-, Grass-, and Rock-Type Egg Event","Battle Villa Challenge"],
-    date: "February 4, 2021",
-    expires: "February 18, 2021"
-  },
-  {
     name: ["Legendary Event: Lurking Shadow [Mewtwo]"],
     date: "February 8, 2021",
     expires: "March 11, 2021"
@@ -85,7 +10,7 @@ export const rumors = [
     expires: "February 26, 2021"
   },
   {
-    name: ["Legendary Event: Give and Take [Xerneas]","Lysandre [gridEX] Poké Fair Scout"],
+    name: ["Legendary Event: Give and Take (Xerneas [gridEX])","Lysandre [gridEX] Poké Fair Scout"],
     date: "February 11, 2021",
     expires: "March 4, 2021"
   },
@@ -108,5 +33,55 @@ export const rumors = [
     name: ["Lance [gridEX] and Sygna Suit Cynthia [gridEX] Poké Fair Scout"],
     date: "February 22, 2021",
     expires: "March 4, 2021"
+  },
+  {
+    name: ["Story Event: Champion Time","Leon [gridEX] Master Fair Scout","Marnie [gridEX] Poké Fair Scout"],
+    date: "February 26, 2021",
+    expires: "March 18, 2021"
+  },
+  {
+    name: ["5★-Select Special Scout ×11"],
+    date: "February 28, 2021",
+    expires: "March 18, 2021"
+  },
+  {
+    name: ["Solo Event: Yell and Dream","Gloria Poké Fair Scout"],
+    date: "March 4, 2021",
+    expires: "March 18, 2021"
+  },
+  {
+    name: ["The Battle Villa Is Open!"],
+    date: "March 11, 2021",
+    expires: "March 25, 2021"
+  },
+  {
+    name: ["Legendary Event: New World Dilemma (Palkia [gridEX])"],
+    date: "March 15, 2021",
+    expires: "April 8, 2021"
+  },
+  {
+    name: ["Lisia Spotlight Scout"],
+    date: "March 15, 2021",
+    expires: "March 29, 2021"
+  },
+  {
+    name: ["Story Event: Shining Heart of Dragons","Iris (Champion) [gridEX] Poké Fair Scout"],
+    date: "March 18, 2021",
+    expires: "April 2, 2021"
+  },
+  {
+    name: ["Blissful Bonanza!","Hilbert [gridEX] and Caitlin Spotlight Scout"],
+    date: "March 22, 2021",
+    expires: "April 2, 2021"
+  },
+  {
+    name: ["Legendary Arena: Latias","Fairy- and Normal-Type Egg Event"],
+    date: "March 31, 2021",
+    expires: "April 14, 2021"
+  },
+  {
+    name: ["Diantha [gridEX] Poké Fair Scout"],
+    date: "March 31, 2021",
+    expires: "April 22, 2021"
   }
 ];
