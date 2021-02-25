c7aaf40efff8a11c69dd6e5a316549dc74a66718
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-02-25 00:02:20 -0500

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,19 +1,27 @@
 export const announced = [
   {
-    name: ["Legendary Arena: Cresselia"],
-    date: "February 18, 2021"
+    name: ["Story Event: Champion Time","Leon Master Fair Scout","Marnie Poké Fair Scout"],
+    date: "February 26, 2021"
   },
   {
-    name: ["The Story Continues!"],
-    date: "February 18, 2021"
+    name: ["5★-Select Special Scout ×11"],
+    date: "February 28, 2021"
   },
   {
-    name: ["Piers Spotlight Scout"],
-    date: "February 18, 2021"
+    name: ["Solo Event: Yell and Dream","Gloria Poké Fair Scout"],
+    date: "March 4, 2021"
   },
   {
-    name: ["Lance 6★ EX and Sygna Suit Cynthia 6★ EX Poké Fair Scout"],
-    date: "February 22, 2021"
+    name: ["Story Event: Shining Heart of Dragons","Iris (Champion) Poké Fair Scout"],
+    date: "March 18, 2021"
+  },
+  {
+    name: ["Blissful Bonanza!"],
+    date: "March 22, 2021"
+  },
+  {
+    name: ["Legendary Arena: Latias","Diantha Poké Fair Scout"],
+    date: "March 31, 2021"
   },
   {
     name: ["6★ EX Skyla Unlocked","6★ EX Korrina Unlocked","6★ EX Barry Unlocked"],
