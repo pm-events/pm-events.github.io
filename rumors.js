8b774bbe59dd8a910f3196c2225d38ee96ad029d
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-03-29 03:48:55 -0400

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,68 +1,8 @@
 export const rumors = [
   {
-    name: ["Legendary Event: Lurking Shadow [Mewtwo]"],
-    date: "February 8, 2021",
-    expires: "March 11, 2021"
-  },
-  {
-    name: ["Grimsley and Karen Spotlight Scout"],
-    date: "February 8, 2021",
-    expires: "February 26, 2021"
-  },
-  {
-    name: ["Legendary Event: Give and Take (Xerneas [grid])","Lysandre [gridEX] Poké Fair Scout"],
-    date: "February 11, 2021",
-    expires: "March 4, 2021"
-  },
-  {
-    name: ["Serena [gridEX] and Clemont Spotlight Scout"],
-    date: "February 15, 2021",
-    expires: "March 4, 2021"
-  },
-  {
-    name: ["Legendary Arena: Cresselia"],
-    date: "February 18, 2021",
-    expires: "March 4, 2021"
-  },
-  {
-    name: ["Piers [gridEX] Spotlight Scout"],
-    date: "February 18, 2021",
-    expires: "March 11, 2021"
-  },
-  {
-    name: ["Lance and Sygna Suit Cynthia (Both [gridEX]) Poké Fair Scout"],
-    date: "February 22, 2021",
-    expires: "March 4, 2021"
-  },
-  {
-    name: ["Story Event: Champion Time","Leon [gridEX] Master Fair Scout","Marnie [gridEX] Poké Fair Scout"],
-    date: "February 26, 2021",
-    expires: "March 18, 2021"
-  },
-  {
-    name: ["5★-Select Special Scout ×11"],
-    date: "February 28, 2021",
-    expires: "March 18, 2021"
-  },
-  {
-    name: ["Solo Event: Yell and Dream","Gloria Poké Fair Scout"],
-    date: "March 4, 2021",
-    expires: "March 18, 2021"
-  },
-  {
-    name: ["The Battle Villa Is Open!"],
-    date: "March 11, 2021",
-    expires: "March 25, 2021"
-  },
-  {
     name: ["Legendary Event: New World Dilemma (Palkia [grid])"],
     date: "March 15, 2021",
     expires: "April 8, 2021"
-  },
-  {
-    name: ["Lisia Spotlight Scout"],
-    date: "March 15, 2021",
-    expires: "March 29, 2021"
   },
   {
     name: ["Story Event: Shining Heart of Dragons","Iris (Champion) [gridEX] Poké Fair Scout"],
@@ -88,5 +28,70 @@ export const rumors = [
     name: ["Diantha [gridEX] Poké Fair Scout"],
     date: "March 31, 2021",
     expires: "April 22, 2021"
+  },
+  {
+    name: ["Special Event: Happy Team Break Day!"],
+    date: "April 1, 2021",
+    expires: "April 2, 2021"
+  },
+  {
+    name: ["Story Event: Pasio Eggsplorers", "May (Spring 2021) [gridEX] & Lopunny Seasonal Scout","Burgh (Spring 2021) [gridEX] & Togepi Seasonal Scout"],
+    date: "April 2, 2021",
+    expires: "April 16, 2021"
+  },
+  {
+    name: ["Champion Stadium: Unova Challenge"],
+    date: "April 5, 2021",
+    expires: "April 12, 2021"
+  },
+  {
+    name: ["Leaf [gridEX] and Gladion Spotlight Scout"],
+    date: "April 5, 2021",
+    expires: "April 17, 2021"
+  },
+  {
+    name: ["Morty, Jasmine, and Glacia Spotlight Scout"],
+    date: "April 5, 2021",
+    expires: "April 22, 2021"
+  },
+  {
+    name: ["The Battle Villa Is Open!","May and Brendan Spotlight Scout"],
+    date: "April 8, 2021",
+    expires: "April 22, 2021"
+  },
+  {
+    name: ["Legendary Event: The Dragon That Rules the Sky (Rayquaza [grid])"],
+    date: "April 8, 2021",
+    expires: "May 18, 2021"
+  },
+  {
+    name: ["The Story Continues!"],
+    date: "April 8, 2021",
+    expires: "April 15, 2021"
+  },
+  {
+    name: ["Story Event: Trials on the Isle","Selene & Rowlet Spotlight Scout","Elio & Popplio Spotlight Scout"],
+    date: "April 15, 2021",
+    expires: "May 2, 2021"
+  },
+  {
+    name: ["Guzma and Lusamine Spotlight Scout"],
+    date: "April 19, 2021",
+    expires: "May 2, 2021"
+  },
+  {
+    name: ["Water- and Electric-Type Egg Event","Legendary Arena: Tornadus"],
+    date: "April 22, 2021",
+    expires: "May 6, 2021"
+  },
+  {
+    name: ["N Poké Fair Scout"],
+    date: "April 22, 2021",
+    expires: "May 20, 2021"
+  },
+  {
+    name: ["Double Feature Spotlight Scout (Volkner & SS Elesa)"],
+    date: "April 26, 2021",
+    expires: "May 6, 2021"
   }
 ];
