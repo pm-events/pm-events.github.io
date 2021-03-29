6386d07a5feede0f1424172a4f4f4418f472509d
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-03-29 03:14:13 -0400

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,21 +1,5 @@
 export const announced = [
   {
-    name: ["Story Event: Shining Heart of Dragons","Iris (Champion) Poké Fair Scout"],
-    date: "March 18, 2021"
-  },
-  {
-    name: ["Blissful Bonanza!"],
-    date: "March 22, 2021"
-  },
-  {
-    name: ["Fairy- and Normal-Type Egg Event"],
-    date: "March 25, 2021"
-  },
-  {
-    name: ["6★ EX Barry Unlocked"],
-    date: "March 29, 2021"
-  },
-  {
     name: ["Legendary Arena: Latias","Diantha Poké Fair Scout"],
     date: "March 31, 2021"
   }
