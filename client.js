e2407256fb97cb64dd1c06dc7bc5e0eb550477c8
antocs <roxasthethund@yahoo.it>
2021-04-11 09:46:20 +0200

    Update client.js

--- client.js
+++ client.js
@@ -24,8 +24,8 @@ document.getElementById("rumors").onclick = () => {
         date: event.split("\n")[1],
         expires: event.split("\n")[2]}
     })
+    console.log(rumors);
     const events = rumors.filter(e => new Date(e.date).getTime() - Date.now() >= 0 || e.date === "TBA").sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-    console.log(rumorstxt);
     const nextEvents = events.filter(e => e.date === events[0].date);
     const futureEvents = events.filter(e => e.date !== events[0].date);
     document.getElementById("rumorstitles").innerHTML = nextEvents.length >= 1 ? "<b style=\"\">" + nextEvents.map(e => e.name.replace(/\[ex\]/g, "<img src=\"./ex.png\"/>").replace(/\[gridEX\]/g, "<img src=\"./grid.png\"/><img src=\"./ex.png\"/>")).join("<br>") + "</b>" : "No rumors yet.";
