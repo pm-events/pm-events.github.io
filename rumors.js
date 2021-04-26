7456ec780695d5440b65015a303fa91030a7b571
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-04-26 16:56:59 -0400

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,41 +1,7 @@
-export const rumorstxt = `Legendary Arena: Latias
-
-2021-03-31 06:00:00 UTC
-2021-04-14 06:00:00 UTC
-
-Diantha [gridEX] Poké Fair Scout
-
-2021-03-31 06:00:00 UTC
-2021-04-22 06:00:00 UTC
-
-Story Event: Pasio Eggsplorers
-
-2021-04-02 06:00:00 UTC
-2021-04-16 06:00:00 UTC
-
-Champion Stadium Unova Challenge
-
-2021-04-05 06:00:00 UTC
-
-The Battle Villa is Open
-
-2021-04-08 06:00:00 UTC
-2021-04-22 06:00:00 UTC
-
-Legendary Event: The Dragon That Rules the Sky (Rayquaza [grid])
+export const rumorstxt = `Legendary Event: The Dragon That Rules the Sky (Rayquaza [grid])
 
 2021-04-08 06:00:00 UTC
 2021-05-18 06:00:00 UTC
-
-Special Rally
-
-2021-04-08 06:00:00 UTC
-2021-04-15 06:00:00 UTC
-
-Training Area Reward Boost Rally
-
-2021-04-10 06:00:00 UTC
-2021-04-15 06:00:00 UTC
 
 Story Event: Trials on the Isle
 
@@ -51,31 +17,6 @@ Legendary Arena Tornadus
 
 2021-04-22 06:00:00 UTC
 2021-05-06 06:00:00 UTC
-
-May (Spring 2021) & Lopunny Seasonal Scout
-
-2021-04-02 06:00:00 UTC
-2021-04-16 06:00:00 UTC
-
-Burgh (Spring 2021) & Togepi Seasonal Scout
-
-2021-04-02 06:00:00 UTC
-2021-04-16 06:00:00 UTC
-
-Leaf [gridEX] and Gladion Spotlight Scout
-
-2021-04-05 06:00:00 UTC
-2021-04-17 06:00:00 UTC
-
-Morty, Jasmine, and Glacia Spotlight Scout
-
-2021-04-05 06:00:00 UTC
-2021-04-22 06:00:00 UTC
-
-May and Brendan Spotlight Scout
-
-2021-04-08 06:00:00 UTC
-2021-04-22 06:00:00 UTC
 
 Elio & Popplio Spotlight Scout
 
@@ -100,4 +41,138 @@ N Poké Fair Scout
 Double Feature Spotlight Scout (Volkner & SS Elesa)
 
 2021-04-26 06:00:00 UTC
-2021-05-06 06:00:00 UTC`;
+2021-05-06 06:00:00 UTC
+
+Raihan Welcome Rally
+
+2021-04-28 06:00:00 UTC
+2021-05-13 06:00:00 UTC
+
+Limited-Time Extra Gem Specials!
+
+2021-04-28 06:00:00 UTC
+2021-05-13 06:00:00 UTC
+
+Time Trial Event: Eternal Rivals
+
+2021-04-28 06:00:00 UTC
+2021-05-16 06:00:00 UTC
+
+Raihan Poké Fair Scout
+
+2021-04-28 06:00:00 UTC
+2021-05-31 06:00:00 UTC
+
+Blissful: Bonanza!
+
+2021-04-30 06:00:00 UTC
+2021-05-07 06:00:00 UTC
+
+5★-Select Special Scout ×11
+
+2021-04-30 06:00:00 UTC
+2021-05-13 06:00:00 UTC
+
+The Battle Villa Is Open!
+
+2021-04-30 06:00:00 UTC
+2021-05-14 06:00:00 UTC
+
+Alder Poké Fair Scout
+
+2021-05-04 06:00:00 UTC
+2021-05-20 06:00:00 UTC
+
+Legendary Event: Father or Foe (Kyurem [grid])
+
+2021-05-04 06:00:00 UTC
+2021-05-20 06:00:00 UTC
+
+Special Rally
+
+2021-05-06 06:00:00 UTC
+2021-05-13 06:00:00 UTC
+
+Special Log-In Bonus
+
+2021-05-06 06:00:00 UTC
+2021-05-16 06:00:00 UTC
+
+Triple Feature Spotlight Scout (Grimsley, Elesa, Burgh)
+
+2021-05-06 06:00:00 UTC
+2021-05-20 06:00:00 UTC
+
+Legendary Arena: Azelf
+
+2021-05-06 06:00:00 UTC
+2021-05-20 06:00:00 UTC
+
+Special Event Phase 1: Weather Alert
+
+2021-05-06 06:00:00 UTC
+2021-05-28 06:00:00 UTC
+
+Nessa Spotlight Scout
+
+2021-05-13 06:00:00 UTC
+2021-05-29 06:00:00 UTC
+
+Solo Event: Splash and Punch
+
+2021-05-13 06:00:00 UTC
+2021-05-29 06:00:00 UTC
+
+Event Gem Special!
+
+2021-05-13 06:00:00 UTC
+2021-05-31 06:00:00 UTC
+
+Bea Spotlight Scout
+
+2021-05-15 06:00:00 UTC
+2021-05-29 06:00:00 UTC
+
+Special Event Phase 2: Land and Sea Awaken
+
+2021-05-17 06:00:00 UTC
+2021-05-28 06:00:00 UTC
+
+Piers Spotlight Scout
+
+2021-05-17 06:00:00 UTC
+2021-05-29 06:00:00 UTC
+
+Legendary Event: Pure Hearts and Rainbow Wings (Ho-Oh [grid])
+
+2021-05-18 06:00:00 UTC
+2021-06-07 06:00:00 UTC
+
+Serena, Hilda, and Ethan Spotlight Scout
+
+2021-05-20 06:00:00 UTC
+2021-06-03 06:00:00 UTC
+
+Legendary Arena: Cobalion
+
+2021-05-20 06:00:00 UTC
+2021-06-03 06:00:00 UTC
+
+Fire-Type Egg Event
+
+2021-05-20 06:00:00 UTC
+2021-06-03 06:00:00 UTC
+
+Training Area Reward Boost Rally
+
+2021-05-25 06:00:00 UTC
+2021-06-01 06:00:00 UTC
+
+Super Spotlight Poké Fair Scout (Steven, SS Cynthia, Lance, maybe Cynthia)
+
+2021-05-25 06:00:00 UTC
+2021-06-08 06:00:00 UTC
+
+Story Event: Two Champions
+2021-05-25 06:00:00 UTC
+2021-06-08 06:00:00 UTC`;
