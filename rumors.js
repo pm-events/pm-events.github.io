f587f7c6d3bf339bb693d2fdd9e5b59f472566ed
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-06-28 19:27:55 -0400

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,84 +1,4 @@
-export const rumorstxt = `Special Event Phase 1: Weather Alert
-
-2021-05-06 06:00:00 UTC
-2021-05-28 06:00:00 UTC
-
-Nessa & Drednaw Spotlight Scout
-
-2021-05-13 06:00:00 UTC
-2021-05-29 06:00:00 UTC
-
-Solo Event: Splash and Punch
-
-2021-05-13 06:00:00 UTC
-2021-05-29 06:00:00 UTC
-
-Event Gem Special!
-
-2021-05-13 06:00:00 UTC
-2021-05-31 06:00:00 UTC
-
-Bea & Sirfetch'd Spotlight Scout
-
-2021-05-15 06:00:00 UTC
-2021-05-29 06:00:00 UTC
-
-Special Event Phase 2: Land and Sea Awaken
-
-2021-05-17 06:00:00 UTC
-2021-05-28 06:00:00 UTC
-
-Piers Spotlight Scout
-
-2021-05-17 06:00:00 UTC
-2021-05-29 06:00:00 UTC
-
-Legendary Event: Pure Hearts and Rainbow Wings (Ho-Oh [grid])
-
-2021-05-18 06:00:00 UTC
-2021-06-07 06:00:00 UTC
-
-Serena, Hilda [ex], and Ethan [ex] Spotlight Scout
-
-2021-05-20 06:00:00 UTC
-2021-06-03 06:00:00 UTC
-
-Legendary Arena: Cobalion
-
-2021-05-20 06:00:00 UTC
-2021-06-03 06:00:00 UTC
-
-Fire-Type Egg Event
-
-2021-05-20 06:00:00 UTC
-2021-06-03 06:00:00 UTC
-
-Training Area Reward Boost Rally
-
-2021-05-25 06:00:00 UTC
-2021-06-01 06:00:00 UTC
-
-Super Spotlight Poké Fair Scout (Steven, SS Cynthia, Lance, Cynthia)
-
-2021-05-25 06:00:00 UTC
-2021-06-08 06:00:00 UTC
-
-Story Event: Two Champions
-
-2021-05-25 06:00:00 UTC
-2021-06-08 06:00:00 UTC
-
-Master Fair Gem Special!
-
-2021-05-31 06:00:00 UTC
-2021-06-11 06:00:00 UTC
-
-Story Event: Masters of Land and Sea
-
-2021-05-31 06:00:00 UTC
-2021-06-14 06:00:00 UTC
-
-Maxie & Groudon Master Fair Scout
+export const rumorstxt = `Maxie & Groudon Master Fair Scout
 
 2021-05-31 06:00:00 UTC
 2021-06-30 06:00:00 UTC
@@ -93,49 +13,10 @@ Gem Special + Present!
 2021-06-01 06:00:00 UTC
 2021-07-01 06:00:00 UTC
 
-Hoenn 5★-Guaranteed Scout
-
-2021-06-02 06:00:00 UTC
-2021-06-21 06:00:00 UTC
-
-Extreme Battle Event: Ancient Brawl
-
-2021-06-03 06:00:00 UTC
-2021-06-21 06:00:00 UTC
-
-Special Rally
-
-2021-06-07 06:00:00 UTC
-2021-06-14 06:00:00 UTC
-
-The Story Continues!
-
-2021-06-07 06:00:00 UTC
-
-The Battle Villa Is Open!
-
-2021-06-07 06:00:00 UTC
-2021-06-21 06:00:00 UTC
-
 Legendary Event: The Beast That Devours the Sun (Solgaleo [grid])
 
 2021-06-07 06:00:00 UTC
 2021-07-26 06:00:00 UTC
-
-Triple Feature Spotlight Scout
-
-2021-06-09 06:00:00 UTC
-2021-06-23 06:00:00 UTC
-
-Legendary Arena: Entei
-
-2021-06-09 06:00:00 UTC
-2021-06-23 06:00:00 UTC
-
-Battle Point Challenge: Daily Region Rotation
-
-2021-06-10 06:00:00 UTC
-2021-06-15 06:00:00 UTC
 
 Lana & Araquanid Spotlight Scout
 
@@ -195,4 +76,114 @@ Story Event: The Star of the Contest
 Legendary Arena: Regirock
 
 2021-06-25 06:00:00 UTC
-2021-07-09 06:00:00 UTC`;
+2021-07-09 06:00:00 UTC
+
+Event Gem Special!
+
+2021-06-30 06:00:00 UTC
+2021-07-15 06:00:00 UTC
+
+Story Event: All Aboard the Victory Train
+
+2021-06-30 06:00:00 UTC
+2021-07-15 06:00:00 UTC
+
+Ingo & Excadrill Poké Fair Scout
+
+2021-06-30 06:00:00 UTC
+2021-07-22 06:00:00 UTC
+
+Emmet & Archeops Poké Fair Scout
+
+2021-06-30 06:00:00 UTC
+2021-07-22 06:00:00 UTC
+
+Gem Special + Present!
+
+2021-07-01 06:00:00 UTC
+2021-08-01 06:00:00 UTC
+
+Unova 5★-Guaranteed Scout
+
+2021-07-02 06:00:00 UTC
+2021-07-15 06:00:00 UTC
+
+The Battle Villa Is Open!
+
+2021-07-05 06:00:00 UTC
+2021-07-19 06:00:00 UTC
+
+Rock-Type Gear Event
+
+2021-07-08 06:00:00 UTC
+2021-07-15 06:00:00 UTC
+
+Olivia [ex] and Kukui Spotlight Scout
+
+2021-07-12 06:00:00 UTC
+2021-07-26 06:00:00 UTC
+
+Legendary Arena: Moltres
+
+2021-07-12 06:00:00 UTC
+2021-07-26 06:00:00 UTC
+
+Rock- and Ground-Type Egg Event
+
+2021-07-12 06:00:00 UTC
+2021-07-26 06:00:00 UTC
+
+Event Gem Special!
+
+2021-07-15 06:00:00 UTC
+2021-07-30 06:00:00 UTC
+
+Gloria & Inteleon Seasonal Tiered Scout
+
+2021-07-15 06:00:00 UTC
+2021-07-31 06:00:00 UTC
+
+Story Event: Beachside Rivalry
+
+2021-07-15 06:00:00 UTC
+2021-07-31 06:00:00 UTC
+
+Marnie & Grimmsnarl Seasonal Tiered Scout
+
+2021-07-17 06:00:00 UTC
+2021-07-31 06:00:00 UTC
+
+Steven and Lyra Seasonal Scout
+
+2021-07-20 06:00:00 UTC
+2021-08-03 06:00:00 UTC
+
+Story Event: Summer Superstars
+
+2021-07-20 06:00:00 UTC
+2021-08-03 06:00:00 UTC
+
+Sync Pair-Up Event: Bugsy & Scyther
+
+2021-07-22 06:00:00 UTC
+2021-08-05 06:00:00 UTC
+
+Special Rally
+
+2021-07-26 06:00:00 UTC
+2021-08-02 06:00:00 UTC
+
+Legendary Event: Lurking Shadow (Mewtwo [grid])
+
+2021-07-26 06:00:00 UTC
+2021-08-19 06:00:00 UTC
+
+Lisia and Glacia Spotlight Scout
+
+2021-07-27 06:00:00 UTC
+2021-08-10 06:00:00 UTC
+
+Legendary Arena: Latios
+
+2021-07-27 06:00:00 UTC
+2021-08-10 06:00:00 UTC`;
