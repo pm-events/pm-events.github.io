0b5ed7cdbb63c9fe4b163ef9ced205736ba4b490
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-07-23 23:26:25 -0400

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,22 +1,6 @@
 export const announced = [
   {
-    name: ["Bea & Sirfetch'd Spotlight Scout"],
-    date: "May 15, 2021"
-  },
-  {
-    name: ["Special Event Phase 2: Land and Sea Awaken"],
-    date: "May 17, 2021"
-  },
-  {
-    name: ["Fire-Type Egg Event","Legendary Arena: Cobalion"],
-    date: "May 20, 2021"
-  },
-  {
-    name: ["Super Spotlight Poké Fair Scout (Steven, SS Cynthia, Lance, Cynthia)","Story Event: Two Champions"],
-    date: "May 25, 2021"
-  },
-  {
-    name: ["Daily Region Rotation Event","Player Character & Regirock","Zinnia & Salamence","Morty & Mismagius"],
-    date: "June 10, 2021"
+    name: ["New Main Story Content Coming Soon!"],
+    date: "July 30, 2021"
   }
 ];
