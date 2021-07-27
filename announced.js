fecd5c5b2cb5e374b5e3d0c2f14b2e8cbb1b2d01
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-07-26 20:57:03 -0400

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,6 +1,30 @@
 export const announced = [
   {
-    name: ["New Main Story Content Coming Soon!"],
+    name: ["PML Arc of the Main Story Concludes!","Solo Event: Lear Takes the Lead","Lear & Hoopa, Rachel & Umbreon, and Sawyer & Honchkrow Sync Pair Scout"],
     date: "July 30, 2021"
+  },
+  {
+    name: ["Daily Type Rotation: Type Team-Up", "Falkner & Swellow Sync Pair Scout"],
+    date: "August 5, 2021"
+  },
+  {
+    name: ["Battle Points Added to Champion Stadium's Master Mode"],
+    date: "August 9, 2021"
+  },
+  {
+    name: ["Run-Up to Two Years Celebration","Legendary Event: Howling Shield of Eternity (Hop & Zamazenta)"],
+    date: "August 12, 2021"
+  },
+  {
+    name: ["Two-Year Run-Up Special Rally","Legendary Event: New World Dilemma (Cyrus & Palkia)", "Legendary Event: Give and Take (Professor Sycamore & Xerneas)", "Legendary Event: Father or Foe (Ghetsis & Kyurem)", "Special Log-In Bonuses & Missions", "Training Area Reward Boost Rally", "Blissful Bonanza!", "Something Special"],
+    date: "August 19, 2021"
+  },
+  {
+    name: ["Legendary Arena: Cresselia"],
+    date: "August 20, 2021"
+  },
+  {
+    name: ["Triple Feature Poke Fair Scout for SS Red, SS Blue, and SS Leaf (72 Hours Only!)", "Evelyn & Entei Added to BP Exchange", "Sync Pair that can use Max Moves"],
+    date: "TBA"
   }
 ];
