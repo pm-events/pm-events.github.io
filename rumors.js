6270d4f0d134b01f57d0dead5a6c19657b7e2b36
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-07-28 23:06:49 -0400

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,137 +1,7 @@
-export const rumorstxt = `Maxie & Groudon Master Fair Scout
-
-2021-05-31 06:00:00 UTC
-2021-06-30 06:00:00 UTC
-
-Archie & Kyogre Master Fair Scout
-
-2021-06-01 06:00:00 UTC
-2021-06-30 06:00:00 UTC
-
-Gem Special + Present!
-
-2021-06-01 06:00:00 UTC
-2021-07-01 06:00:00 UTC
-
-Legendary Event: The Beast That Devours the Sun (Solgaleo [grid])
-
-2021-06-07 06:00:00 UTC
-2021-07-26 06:00:00 UTC
-
-Lana & Araquanid Spotlight Scout
-
-2021-06-11 06:00:00 UTC
-2021-07-01 06:00:00 UTC
-
-Event Gem Special!
-
-2021-06-11 06:00:00 UTC
-2021-07-01 06:00:00 UTC
-
-Solo Event: Town Square Street Fair
-
-2021-06-11 06:00:00 UTC
-2021-07-01 06:00:00 UTC
-
-Kiawe & Alolan Marowak Spotlight Scout
-
-2021-06-13 06:00:00 UTC
-2021-07-01 06:00:00 UTC
-
-Hala & Crabominable Spotlight Scout
-
-2021-06-15 06:00:00 UTC
-2021-07-01 06:00:00 UTC
-
-The Masked Royal & Incineroar Spotlight Scout
-
-2021-06-17 06:00:00 UTC
-2021-07-01 06:00:00 UTC
-
-Grass- and Poison-Type Egg Event
-
-2021-06-21 06:00:00 UTC
-2021-07-05 06:00:00 UTC
-
-Triple Feature Spotlight Scout
-
-2021-06-21 06:00:00 UTC
-2021-07-08 06:00:00 UTC
-
-5★-Select Special Scout ×11
-
-2021-06-22 06:00:00 UTC
-2021-07-06 06:00:00 UTC
-
-Training Area Reward Boost Rally
-
-2021-06-24 06:00:00 UTC
-2021-06-30 06:00:00 UTC
-
-Story Event: The Star of the Contest
-
-2021-06-24 06:00:00 UTC
-2021-07-08 06:00:00 UTC
-
-Legendary Arena: Regirock
-
-2021-06-25 06:00:00 UTC
-2021-07-09 06:00:00 UTC
-
-Event Gem Special!
-
-2021-06-30 06:00:00 UTC
-2021-07-15 06:00:00 UTC
-
-Story Event: All Aboard the Victory Train
-
-2021-06-30 06:00:00 UTC
-2021-07-15 06:00:00 UTC
-
-Ingo & Excadrill Poké Fair Scout
-
-2021-06-30 06:00:00 UTC
-2021-07-22 06:00:00 UTC
-
-Emmet & Archeops Poké Fair Scout
-
-2021-06-30 06:00:00 UTC
-2021-07-22 06:00:00 UTC
-
-Gem Special + Present!
+export const rumorstxt = `Gem Special + Present!
 
 2021-07-01 06:00:00 UTC
 2021-08-01 06:00:00 UTC
-
-Unova 5★-Guaranteed Scout
-
-2021-07-02 06:00:00 UTC
-2021-07-15 06:00:00 UTC
-
-The Battle Villa Is Open!
-
-2021-07-05 06:00:00 UTC
-2021-07-19 06:00:00 UTC
-
-Rock-Type Gear Event
-
-2021-07-08 06:00:00 UTC
-2021-07-15 06:00:00 UTC
-
-Olivia [ex] and Kukui Spotlight Scout
-
-2021-07-12 06:00:00 UTC
-2021-07-26 06:00:00 UTC
-
-Legendary Arena: Moltres
-
-2021-07-12 06:00:00 UTC
-2021-07-26 06:00:00 UTC
-
-Rock- and Ground-Type Egg Event
-
-2021-07-12 06:00:00 UTC
-2021-07-26 06:00:00 UTC
 
 Event Gem Special!
 
@@ -186,4 +56,139 @@ Lisia and Glacia Spotlight Scout
 Legendary Arena: Latios
 
 2021-07-27 06:00:00 UTC
-2021-08-10 06:00:00 UTC`;
+2021-08-10 06:00:00 UTC
+
+The Story Continues!
+
+2021-07-30 06:00:00 UTC
+2021-08-06 06:00:00 UTC
+
+Event Gem Special!
+
+2021-07-30 06:00:00 UTC
+2021-08-12 06:00:00 UTC
+
+Triple Feature Poké Fair Scout (Lear / Rachel / Sawyer)
+
+2021-07-30 06:00:00 UTC
+2021-08-13 06:00:00 UTC
+
+Solo Event: Lear Takes the Lead
+
+2021-07-30 06:00:00 UTC
+2021-08-13 06:00:00 UTC
+
+Gem Special + Present!
+
+2021-08-01 06:00:00 UTC
+2021-09-01 06:00:00 UTC
+
+The Battle Villa Is Open!
+
+2021-08-02 06:00:00 UTC
+2021-08-16 06:00:00 UTC
+
+Flying- and Psychic-Type Egg Event
+
+2021-08-03 06:00:00 UTC
+2021-08-17 06:00:00 UTC
+
+Legendary Arena: Tapu Bulu
+
+2021-08-05 06:00:00 UTC
+2021-08-19 06:00:00 UTC
+
+Falkner Spotlight Scout
+
+2021-08-05 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Daily Type Rotation: Type Team-Up
+
+2021-08-05 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Sidney Spotlight Scout
+
+2021-08-09 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Sygna Suit Leon Poké Fair Scout
+
+2021-08-12 06:00:00 UTC
+2021-09-16 06:00:00 UTC
+
+Legendary Event: Howling Shield of Eternity
+
+2021-08-12 06:00:00 UTC
+2021-09-16 06:00:00 UTC
+
+Run-Up to Two Years Rally
+
+2021-08-12 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Run-Up to Two Years Gem Specials!
+
+2021-08-12 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Gloria Poké Fair Scout
+
+2021-08-16 06:00:00 UTC
+2021-09-02 06:00:00 UTC
+
+5★-Select Special Scout ×11
+
+2021-08-17 06:00:00 UTC
+2021-09-02 06:00:00 UTC
+
+10-Pair Scout (Max 100 Pairs)
+
+2021-08-19 06:00:00 UTC
+2021-10-07 06:00:00 UTC
+
+Legendary Event: Give and Take (Professor Sycamore & Xerneas)
+
+2021-08-19 06:00:00 UTC
+2021-09-16 06:00:00 UTC
+
+Legendary Event: Father or Foe (Ghetsis & Kyurem)
+
+2021-08-19 06:00:00 UTC
+2021-09-16 06:00:00 UTC
+
+Legendary Event: New World Dilemma (Cyrus & Palkia)
+
+2021-08-19 06:00:00 UTC
+2021-09-16 06:00:00 UTC
+
+Two-Year Run-Up Special Rally
+
+2021-08-19 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Blissful: Bonanza!
+
+2021-08-19 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Training Area Reward Boost Rally
+
+2021-08-19 06:00:00 UTC
+2021-08-28 06:00:00 UTC
+
+Lysandre Poké Fair Scout
+
+2021-08-20 06:00:00 UTC
+2021-09-04 06:00:00 UTC
+
+Legendary Arena: Cresselia
+
+2021-08-20 06:00:00 UTC
+2021-09-04 06:00:00 UTC
+
+Sabrina [ex] and Phoebe [ex] Spotlight Scout
+
+2021-08-23 06:00:00 UTC
+2021-09-04 06:00:00 UTC`;
