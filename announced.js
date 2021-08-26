47074f0efe2ac4d9c5680f550997b75750c07aaa
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-08-26 04:40:50 -0400

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,22 +1,18 @@
 export const announced = [
   {
-    name: ["Run-Up to Two Years Celebration","Legendary Event: Howling Shield of Eternity (Hop & Zamazenta)"],
-    date: "August 12, 2021"
-  },
-  {
-    name: ["Two-Year Run-Up Special Rally","Legendary Event: New World Dilemma (Cyrus & Palkia)", "Legendary Event: Give and Take (Professor Sycamore & Xerneas)", "Legendary Event: Father or Foe (Ghetsis & Kyurem)", "Special Log-In Bonuses & Missions", "Training Area Reward Boost Rally", "Blissful Bonanza!", "Something Special"],
-    date: "August 19, 2021"
-  },
-  {
-    name: ["Legendary Arena: Cresselia"],
-    date: "August 20, 2021"
+    name: ["New Areas Added to the Training Area", "Ticket Scout Update"],
+    date: "August 28, 2021"
   },
   {
     name: ["Two-year Anniversary"],
     date: "August 29, 2021"
   },
   {
-    name: ["Daily log-in bonus update", "Zero stamina consumption for the Evolution Area and Sync Orb Area", "EggDex"],
+    name: ["Daily Log-in Bonus Reward Update"],
+    date: "September 2, 2021"
+  },
+  {
+    name: ["EggDex"],
     date: "TBA"
   }
 ];
