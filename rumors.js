214031b4c8cf8958afd07928cad06dd02710e7e2
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-09-28 03:52:44 -0400

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,42 +1,7 @@
-export const rumorstxt = `Sygna Suit Leon Poké Fair Scout
-
-2021-08-12 06:00:00 UTC
-2021-09-16 06:00:00 UTC
-
-Legendary Event: Howling Shield of Eternity
-
-2021-08-12 06:00:00 UTC
-2021-09-16 06:00:00 UTC
-
-10-Pair Scout (Max 100 Pairs)
+export const rumorstxt = `10-Pair Scout (Max 100 Pairs)
 
 2021-08-19 06:00:00 UTC
 2021-10-07 06:00:00 UTC
-
-Legendary Event: Give and Take (Professor Sycamore & Xerneas)
-
-2021-08-19 06:00:00 UTC
-2021-09-16 06:00:00 UTC
-
-Legendary Event: Father or Foe (Ghetsis & Kyurem)
-
-2021-08-19 06:00:00 UTC
-2021-09-16 06:00:00 UTC
-
-Legendary Event: New World Dilemma (Cyrus & Palkia)
-
-2021-08-19 06:00:00 UTC
-2021-09-16 06:00:00 UTC
-
-5★-Select Poké Fair Scout ×11
-
-2021-08-28 06:00:00 UTC
-2021-09-16 06:00:00 UTC
-
-Two-Year Anniversary Gem Specials!
-
-2021-08-28 06:00:00 UTC
-2021-09-16 06:00:00 UTC
 
 Special Sync Pair Event: Misty (Swimsuit) & Psyduck
 
@@ -58,65 +23,10 @@ N (Anniversary 2021) & Reshiram Master Fair Scout
 2021-08-30 06:00:00 UTC
 2021-09-30 06:00:00 UTC
 
-Extreme Battle Event: Unity Gala Challenge
-
-2021-09-01 06:00:00 UTC
-2021-09-23 06:00:00 UTC
-
 Steven (Anniversary 2021) & Rayquaza Master Fair Scout
 
 2021-09-01 06:00:00 UTC
 2021-09-30 06:00:00 UTC
-
-The Battle Villa Is Open!
-
-2021-09-02 06:00:00 UTC
-2021-09-16 06:00:00 UTC
-
-Two-Year Anniversary Rerun Event - The Girl in Black and White
-
-2021-09-03 06:00:00 UTC
-2021-09-17 06:00:00 UTC
-
-Two-Year Anniversary Rerun Event - Evolution Material Mining!
-
-2021-09-05 06:00:00 UTC
-2021-09-17 06:00:00 UTC
-
-Two-Year Anniversary Rerun Event - Family Ties
-
-2021-09-07 06:00:00 UTC
-2021-09-17 06:00:00 UTC
-
-Two-Year Anniversary Rerun Event - The Eccentric Electrician
-
-2021-09-09 06:00:00 UTC
-2021-09-17 06:00:00 UTC
-
-Two-Year Anniversary Rerun Event - Dazzling Dizzying Battle Stage
-
-2021-09-11 06:00:00 UTC
-2021-09-17 06:00:00 UTC
-
-Two-Year Anniversary Rerun Event - The Ideal Formula
-
-2021-09-13 06:00:00 UTC
-2021-09-17 06:00:00 UTC
-
-Dragon- and Water-Type Egg Event
-
-2021-09-09 06:00:00 UTC
-2021-09-23 06:00:00 UTC
-
-Super Spotlight Seasonal Scout (Dawn, Erika, Lance, Hilbert, Lyra, Siebold)
-
-2021-09-10 06:00:00 UTC
-2021-09-17 06:00:00 UTC
-
-Legendary Arena: Cobalion
-
-2021-09-10 06:00:00 UTC
-2021-09-24 06:00:00 UTC
 
 Villain Event Prelude: Looming Shadow of Kanto (Mewtwo [ex] Unlocked)
 
@@ -186,4 +96,129 @@ Special Rally
 Training Area Reward Boost Rally
 
 2021-09-24 06:00:00 UTC
-2021-10-01 06:00:00 UTC`;
+2021-10-01 06:00:00 UTC
+
+Event Gem Special!
+
+2021-09-30 06:00:00 UTC
+2021-10-14 06:00:00 UTC
+
+Sygna Suit Misty Spotlight Scout
+
+2021-09-30 06:00:00 UTC
+2021-10-16 06:00:00 UTC
+
+The Story Continues! Villain Arc Update
+
+2021-09-30 06:00:00 UTC
+2021-10-16 06:00:00 UTC
+
+Villain Event: Spreading Shadows (Mega Mewtwo Y Unlocked)
+
+2021-09-30 06:00:00 UTC
+2021-10-16 06:00:00 UTC
+
+Gem Special + Present!
+
+2021-10-01 06:00:00 UTC
+2021-11-01 06:00:00 UTC
+
+Sygna Suit Erika Spotlight Scout
+
+2021-10-01 06:00:00 UTC
+2021-10-16 06:00:00 UTC
+
+Extreme Battle Event: Giovanni’s Malice
+
+2021-10-01 06:00:00 UTC
+2021-10-16 06:00:00 UTC
+
+5★-Select Special Scout ×11
+
+2021-10-04 06:00:00 UTC
+2021-10-18 06:00:00 UTC
+
+Legendary Arena: Entei
+
+2021-10-04 06:00:00 UTC
+2021-10-18 06:00:00 UTC
+
+Special Rally
+
+2021-10-07 06:00:00 UTC
+2021-10-14 06:00:00 UTC
+
+Training Area Reward Boost Rally
+
+2021-10-07 06:00:00 UTC
+2021-10-14 06:00:00 UTC
+
+Solo Event: Mushroom Mania
+
+2021-10-07 06:00:00 UTC
+2021-10-14 06:00:00 UTC
+
+Champion Stadium: Alola Challenge
+
+2021-10-11 06:00:00 UTC
+2021-10-18 06:00:00 UTC
+
+Morty Seasonal Tiered Scout
+
+2021-10-14 06:00:00 UTC
+2021-11-05 06:00:00 UTC
+
+Story Event: Midnight Spookfest
+
+2021-10-14 06:00:00 UTC
+2021-11-05 06:00:00 UTC
+
+Event Gem Special!
+
+2021-10-14 06:00:00 UTC
+2021-10-29 06:00:00 UTC
+
+Caitlin Seasonal Tiered Scout
+
+2021-10-16 06:00:00 UTC
+2021-11-05 06:00:00 UTC
+
+Legendary Event: Pure Hearts and Rainbow Wings
+
+2021-10-16 06:00:00 UTC
+2021-11-22 06:00:00 UTC
+
+The Battle Villa Is Open!
+
+2021-10-19 06:00:00 UTC
+2021-11-02 06:00:00 UTC
+
+Acerola and Hilbert Seasonal Scout
+
+2021-10-20 06:00:00 UTC
+2021-11-04 06:00:00 UTC
+
+Story Event: Fight or Fright
+
+2021-10-20 06:00:00 UTC
+2021-11-04 06:00:00 UTC
+
+Legendary Arena: Uxie
+
+2021-10-22 06:00:00 UTC
+2021-11-05 06:00:00 UTC
+
+Allister Spotlight Scout
+
+2021-10-22 06:00:00 UTC
+2021-11-08 06:00:00 UTC
+
+Darach Spotlight Scout
+
+2021-10-25 06:00:00 UTC
+2021-11-08 06:00:00 UTC
+
+Ghost- and Poison-Type Egg Event
+
+2021-10-25 06:00:00 UTC
+2021-11-08 06:00:00 UTC`;
