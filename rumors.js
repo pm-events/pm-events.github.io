7dc481b55878b94013acbcbc9eefc061ee4b3726
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-10-27 04:23:08 -0400

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,167 +1,17 @@
-export const rumorstxt = `10-Pair Scout (Max 100 Pairs)
-
-2021-08-19 06:00:00 UTC
-2021-10-07 06:00:00 UTC
-
-Special Sync Pair Event: Misty (Swimsuit) & Psyduck
-
-2021-08-28 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-Lillie (Anniversary 2021) & Lunala Master Fair Scout
-
-2021-08-28 06:00:00 UTC
-2021-09-30 06:00:00 UTC
-
-Story Event: Hearts United
-
-2021-08-28 06:00:00 UTC
-2021-09-30 06:00:00 UTC
-
-N (Anniversary 2021) & Reshiram Master Fair Scout
-
-2021-08-30 06:00:00 UTC
-2021-09-30 06:00:00 UTC
-
-Steven (Anniversary 2021) & Rayquaza Master Fair Scout
-
-2021-09-01 06:00:00 UTC
-2021-09-30 06:00:00 UTC
-
-Villain Event Prelude: Looming Shadow of Kanto (Mewtwo [ex] Unlocked)
-
-2021-09-16 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-Legendary Event: Lurking Shadow (Mewtwo)
-
-2021-09-16 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-Event Gem Special!
-
-2021-09-16 06:00:00 UTC
-2021-09-30 06:00:00 UTC
-
-Red & Snorlax Poké Fair Scout
+export const rumorstxt = `Red & Snorlax Poké Fair Scout
 
 2021-09-16 06:00:00 UTC
 2021-10-30 06:00:00 UTC
-
-5★-Guaranteed Kanto Special Scout (Blue, Leaf, Sabrina)
-
-2021-09-18 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-Extreme Battle Event: Team Rocket Takedown
-
-2021-09-18 06:00:00 UTC
-2021-09-30 06:00:00 UTC
 
 Blue (Classic) & Aerodactyl Poké Fair Scout
 
 2021-09-18 06:00:00 UTC
 2021-10-30 06:00:00 UTC
 
-Marnie Poké Fair Scout
-
-2021-09-21 06:00:00 UTC
-2021-10-05 06:00:00 UTC
-
-Legendary Arena: Tornadus
-
-2021-09-21 06:00:00 UTC
-2021-10-05 06:00:00 UTC
-
-Triple Feature Spotlight Scout (Volkner, SS Elesa, Clemont)
-
-2021-09-23 06:00:00 UTC
-2021-10-05 06:00:00 UTC
-
-Blissful: Bonanza!
-
-2021-09-23 06:00:00 UTC
-2021-09-30 06:00:00 UTC
-
-Ghost-Type Gear Event
-
-2021-09-23 06:00:00 UTC
-2021-09-30 06:00:00 UTC
-
-Special Rally
-
-2021-09-24 06:00:00 UTC
-2021-10-01 06:00:00 UTC
-
-Training Area Reward Boost Rally
-
-2021-09-24 06:00:00 UTC
-2021-10-01 06:00:00 UTC
-
-Event Gem Special!
-
-2021-09-30 06:00:00 UTC
-2021-10-14 06:00:00 UTC
-
-Sygna Suit Misty Spotlight Scout
-
-2021-09-30 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-The Story Continues! Villain Arc Update
-
-2021-09-30 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-Villain Event: Spreading Shadows (Mega Mewtwo Y Unlocked)
-
-2021-09-30 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
 Gem Special + Present!
 
 2021-10-01 06:00:00 UTC
 2021-11-01 06:00:00 UTC
-
-Sygna Suit Erika Spotlight Scout
-
-2021-10-01 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-Extreme Battle Event: Giovanni’s Malice
-
-2021-10-01 06:00:00 UTC
-2021-10-16 06:00:00 UTC
-
-5★-Select Special Scout ×11
-
-2021-10-04 06:00:00 UTC
-2021-10-18 06:00:00 UTC
-
-Legendary Arena: Entei
-
-2021-10-04 06:00:00 UTC
-2021-10-18 06:00:00 UTC
-
-Special Rally
-
-2021-10-07 06:00:00 UTC
-2021-10-14 06:00:00 UTC
-
-Training Area Reward Boost Rally
-
-2021-10-07 06:00:00 UTC
-2021-10-14 06:00:00 UTC
-
-Solo Event: Mushroom Mania
-
-2021-10-07 06:00:00 UTC
-2021-10-14 06:00:00 UTC
-
-Champion Stadium: Alola Challenge
-
-2021-10-11 06:00:00 UTC
-2021-10-18 06:00:00 UTC
 
 Morty Seasonal Tiered Scout
 
@@ -221,4 +71,93 @@ Darach Spotlight Scout
 Ghost- and Poison-Type Egg Event
 
 2021-10-25 06:00:00 UTC
-2021-11-08 06:00:00 UTC`;
+2021-11-08 06:00:00 UTC
+
+Diantha Seasonal Tiered Scout
+
+2021-10-29 06:00:00 UTC
+2021-12-10 06:00:00 UTC
+
+Costume Event: Poké War Games
+
+2021-10-29 06:00:00 UTC
+2021-12-10 06:00:00 UTC
+
+Event Gem Special!
+
+2021-10-29 06:00:00 UTC
+2021-11-11 06:00:00 UTC
+
+Gem Special + Present!
+
+2021-11-01 06:00:00 UTC
+2021-12-01 06:00:00 UTC
+
+Hilda Seasonal Tiered Scout
+
+2021-11-04 06:00:00 UTC
+2021-12-10 06:00:00 UTC
+
+Fighting-Type Gear Event
+
+2021-11-08 06:00:00 UTC
+2021-11-15 06:00:00 UTC
+
+The Battle Villa Is Open!
+
+2021-11-08 06:00:00 UTC
+2021-11-22 06:00:00 UTC
+
+Diantha and Iris Poké Fair Scout
+
+2021-11-09 06:00:00 UTC
+2021-11-23 06:00:00 UTC
+
+Legendary Arena: Latias
+
+2021-11-09 06:00:00 UTC
+2021-11-23 06:00:00 UTC
+
+Guzma Seasonal Tiered Scout
+
+2021-11-11 06:00:00 UTC
+2021-12-10 06:00:00 UTC
+
+Event Gem Special!
+
+2021-11-11 06:00:00 UTC
+2021-11-30 06:00:00 UTC
+
+Fighting- and Bug-Type Egg Event
+
+2021-11-15 06:00:00 UTC
+2021-11-29 06:00:00 UTC
+
+Jasmine Seasonal Tiered Scout
+
+2021-11-18 06:00:00 UTC
+2021-12-10 06:00:00 UTC
+
+Legendary Arena: Regirock
+2021-11-19 06:00:00 UTC
+2021-12-03 06:00:00 UTC
+
+Special Rally
+
+2021-11-22 06:00:00 UTC
+2021-11-30 06:00:00 UTC
+
+Training Area Reward Boost Rally
+
+2021-11-22 06:00:00 UTC
+2021-11-30 06:00:00 UTC
+
+Special Monthly Event: Pokémon Masters Day
+
+2021-11-25 06:00:00 UTC
+2021-11-26 06:00:00 UTC
+
+Blissful: Bonanza!
+
+2021-11-26 06:00:00 UTC
+2021-12-03 06:00:00 UTC`;
