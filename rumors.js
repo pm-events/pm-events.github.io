cda7d989f4e1f1b49c753c68109960dd909abd74
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-11-24 18:25:17 -0500

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,24 +1,4 @@
-export const rumorstxt = `Legendary Event: Pure Hearts and Rainbow Wings
-
-2021-10-16 06:00:00 UTC
-2021-11-22 06:00:00 UTC
-
-Allister Spotlight Scout
-
-2021-10-22 06:00:00 UTC
-2021-11-08 06:00:00 UTC
-
-Darach Spotlight Scout
-
-2021-10-25 06:00:00 UTC
-2021-11-08 06:00:00 UTC
-
-Ghost- and Poison-Type Egg Event
-
-2021-10-25 06:00:00 UTC
-2021-11-08 06:00:00 UTC
-
-Diantha Seasonal Tiered Scout
+export const rumorstxt = `Diantha Seasonal Tiered Scout
 
 2021-10-29 06:00:00 UTC
 2021-12-10 06:00:00 UTC
@@ -27,11 +7,6 @@ Costume Event: Poké War Games
 
 2021-10-29 06:00:00 UTC
 2021-12-10 06:00:00 UTC
-
-Event Gem Special!
-
-2021-10-29 06:00:00 UTC
-2021-11-11 06:00:00 UTC
 
 Gem Special + Present!
 
@@ -42,26 +17,6 @@ Hilda Seasonal Tiered Scout
 
 2021-11-04 06:00:00 UTC
 2021-12-10 06:00:00 UTC
-
-Fighting-Type Gear Event
-
-2021-11-08 06:00:00 UTC
-2021-11-15 06:00:00 UTC
-
-The Battle Villa Is Open!
-
-2021-11-08 06:00:00 UTC
-2021-11-22 06:00:00 UTC
-
-Diantha and Iris Poké Fair Scout
-
-2021-11-09 06:00:00 UTC
-2021-11-23 06:00:00 UTC
-
-Legendary Arena: Latias
-
-2021-11-09 06:00:00 UTC
-2021-11-23 06:00:00 UTC
 
 Guzma Seasonal Tiered Scout
 
@@ -111,4 +66,149 @@ Pokémon Masters Day One-Time-Only Happy Scout
 Blissful: Bonanza!
 
 2021-11-26 06:00:00 UTC
-2021-12-03 06:00:00 UTC`;
+2021-12-03 06:00:00 UTC
+
+New chapter added to the villain arc!
+
+2021-11-26 06:00:00 UTC
+2021-12-03 06:00:00 UTC
+
+Grimsley Spotlight Scout
+
+2021-11-26 06:00:00 UTC
+2021-12-10 06:00:00 UTC
+
+Master Fair Gem Special!
+
+2021-11-30 06:00:00 UTC
+2021-12-16 06:00:00 UTC
+
+Story Event: The Light Devourer
+
+2021-11-30 06:00:00 UTC
+2021-12-16 06:00:00 UTC
+
+Sygna Suit Lusamine Master Fair Scout
+
+2021-11-30 06:00:00 UTC
+2021-12-29 06:00:00 UTC
+
+Gem Special + Present!
+
+2021-12-01 06:00:00 UTC
+2022-01-01 06:00:00 UTC
+
+5★-Select Special Scout ×11
+
+2021-12-02 06:00:00 UTC
+2021-12-16 06:00:00 UTC
+
+Extreme Battle Event: Light Up the Dark
+
+2021-12-02 06:00:00 UTC
+2021-12-16 06:00:00 UTC
+
+The Battle Villa Is Open!
+
+2021-12-03 06:00:00 UTC
+2021-12-17 06:00:00 UTC
+
+Electric- and Psychic-Type Egg Event
+
+2021-12-03 06:00:00 UTC
+2021-12-17 06:00:00 UTC
+
+Silver & Ho-Oh: Pure Hearts and Rainbow Wings
+
+2021-12-04 06:00:00 UTC
+2021-12-11 06:00:00 UTC
+
+Special Rally
+
+2021-12-06 06:00:00 UTC
+2021-12-13 06:00:00 UTC
+
+Training Area Reward Boost Rally
+
+2021-12-06 06:00:00 UTC
+2021-12-13 06:00:00 UTC
+
+Elesa Poké Fair Scout
+
+2021-12-09 06:00:00 UTC
+2021-12-23 06:00:00 UTC
+
+Legendary Arena: Tornadus
+
+2021-12-09 06:00:00 UTC
+2021-12-23 06:00:00 UTC
+
+Solo Event: For the Fans
+
+2021-12-09 06:00:00 UTC
+2021-12-23 06:00:00 UTC
+
+Triple Feature Spotlight Scout
+
+2021-12-11 06:00:00 UTC
+2021-12-23 06:00:00 UTC
+
+Zinnia & Rayquaza: The Dragon That Rules the Sky
+
+2021-12-14 06:00:00 UTC
+2021-12-21 06:00:00 UTC
+
+Leon Seasonal Tiered Scout
+
+2021-12-16 06:00:00 UTC
+2022-01-01 06:00:00 UTC
+
+Story Event: Winter Wishes
+
+2021-12-16 06:00:00 UTC
+2022-01-01 06:00:00 UTC
+
+Event Gem Special!
+
+2021-12-16 06:00:00 UTC
+2021-12-29 06:00:00 UTC
+
+Nessa Seasonal Tiered Scout
+
+2021-12-18 06:00:00 UTC
+2022-01-01 06:00:00 UTC
+
+Super Spotlight Seasonal Scout
+
+2021-12-22 06:00:00 UTC
+2022-01-05 06:00:00 UTC
+
+Story Event: Deck the Halls
+
+2021-12-22 06:00:00 UTC
+2022-01-05 06:00:00 UTC
+
+Sonia Spotlight Scout
+
+2021-12-23 06:00:00 UTC
+2022-01-08 06:00:00 UTC
+
+Legendary Arena: Latios
+
+2021-12-24 06:00:00 UTC
+2022-01-08 06:00:00 UTC
+
+Cyrus & Palkia: New World Dilemma
+
+2021-12-24 06:00:00 UTC
+2021-12-31 06:00:00 UTC
+
+Special Monthly Event: Pokémon Masters Day
+
+2021-12-25 06:00:00 UTC
+2021-12-26 06:00:00 UTC
+
+Pokémon Masters Day One-Time-Only Happy Scout
+
+2021-12-25 06:00:00 UTC
+2021-12-26 06:00:00 UTC`;
