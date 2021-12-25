da12e4f78351d1276fd2cbf466aad368eef74461
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2021-12-25 17:44:20 -0500

    Update rumors.js

--- rumors.js
+++ rumors.js
@@ -1,94 +1,4 @@
-export const rumorstxt = `Diantha Seasonal Tiered Scout
-
-2021-10-29 06:00:00 UTC
-2021-12-10 06:00:00 UTC
-
-Costume Event: Poké War Games
-
-2021-10-29 06:00:00 UTC
-2021-12-10 06:00:00 UTC
-
-Gem Special + Present!
-
-2021-11-01 06:00:00 UTC
-2021-12-01 06:00:00 UTC
-
-Hilda Seasonal Tiered Scout
-
-2021-11-04 06:00:00 UTC
-2021-12-10 06:00:00 UTC
-
-Guzma Seasonal Tiered Scout
-
-2021-11-11 06:00:00 UTC
-2021-12-10 06:00:00 UTC
-
-Event Gem Special!
-
-2021-11-11 06:00:00 UTC
-2021-11-30 06:00:00 UTC
-
-Fighting- and Bug-Type Egg Event
-
-2021-11-15 06:00:00 UTC
-2021-11-29 06:00:00 UTC
-
-Jasmine Seasonal Tiered Scout
-
-2021-11-18 06:00:00 UTC
-2021-12-10 06:00:00 UTC
-
-Legendary Arena: Regirock
-
-2021-11-19 06:00:00 UTC
-2021-12-03 06:00:00 UTC
-
-Special Rally
-
-2021-11-22 06:00:00 UTC
-2021-11-30 06:00:00 UTC
-
-Training Area Reward Boost Rally
-
-2021-11-22 06:00:00 UTC
-2021-11-30 06:00:00 UTC
-
-Special Monthly Event: Pokémon Masters Day
-
-2021-11-25 06:00:00 UTC
-2021-11-26 06:00:00 UTC
-
-Pokémon Masters Day One-Time-Only Happy Scout
-
-2021-11-25 06:00:00 UTC
-2021-11-26 06:00:00 UTC
-
-Blissful: Bonanza!
-
-2021-11-26 06:00:00 UTC
-2021-12-03 06:00:00 UTC
-
-New chapter added to the villain arc!
-
-2021-11-26 06:00:00 UTC
-2021-12-03 06:00:00 UTC
-
-Grimsley Spotlight Scout
-
-2021-11-26 06:00:00 UTC
-2021-12-10 06:00:00 UTC
-
-Master Fair Gem Special!
-
-2021-11-30 06:00:00 UTC
-2021-12-16 06:00:00 UTC
-
-Story Event: The Light Devourer
-
-2021-11-30 06:00:00 UTC
-2021-12-16 06:00:00 UTC
-
-Sygna Suit Lusamine Master Fair Scout
+export const rumorstxt = `Sygna Suit Lusamine Master Fair Scout
 
 2021-11-30 06:00:00 UTC
 2021-12-29 06:00:00 UTC
@@ -97,66 +7,6 @@ Gem Special + Present!
 
 2021-12-01 06:00:00 UTC
 2022-01-01 06:00:00 UTC
-
-5★-Select Special Scout ×11
-
-2021-12-02 06:00:00 UTC
-2021-12-16 06:00:00 UTC
-
-Extreme Battle Event: Light Up the Dark
-
-2021-12-02 06:00:00 UTC
-2021-12-16 06:00:00 UTC
-
-The Battle Villa Is Open!
-
-2021-12-03 06:00:00 UTC
-2021-12-17 06:00:00 UTC
-
-Electric- and Psychic-Type Egg Event
-
-2021-12-03 06:00:00 UTC
-2021-12-17 06:00:00 UTC
-
-Silver & Ho-Oh: Pure Hearts and Rainbow Wings
-
-2021-12-04 06:00:00 UTC
-2021-12-11 06:00:00 UTC
-
-Special Rally
-
-2021-12-06 06:00:00 UTC
-2021-12-13 06:00:00 UTC
-
-Training Area Reward Boost Rally
-
-2021-12-06 06:00:00 UTC
-2021-12-13 06:00:00 UTC
-
-Elesa Poké Fair Scout
-
-2021-12-09 06:00:00 UTC
-2021-12-23 06:00:00 UTC
-
-Legendary Arena: Tornadus
-
-2021-12-09 06:00:00 UTC
-2021-12-23 06:00:00 UTC
-
-Solo Event: For the Fans
-
-2021-12-09 06:00:00 UTC
-2021-12-23 06:00:00 UTC
-
-Triple Feature Spotlight Scout
-
-2021-12-11 06:00:00 UTC
-2021-12-23 06:00:00 UTC
-
-Zinnia & Rayquaza: The Dragon That Rules the Sky
-
-2021-12-14 06:00:00 UTC
-2021-12-21 06:00:00 UTC
 
 Leon Seasonal Tiered Scout
 
@@ -223,7 +73,7 @@ Villain Event Prelude: Galactic Nightmare
 2021-12-27 06:00:00 UTC
 2022-01-13 06:00:00 UTC
 
-Sygna Suit Cynthia Master Fair Scout
+Sygna Suit Renegade Cynthia Master Fair Scout
 
 2021-12-27 06:00:00 UTC
 2022-02-28 06:00:00 UTC
