00a71a0abe0b1c7cac4f8681c324c7f2f0d84692
VoltageMiner <10393638+VoltageMiner@users.noreply.github.com>
2022-03-13 21:34:30 -0400

    Update announced.js

--- announced.js
+++ announced.js
@@ -1,30 +1,6 @@
 export const announced = [
   {
-    name: ["Music Coin Challenge Daily Battle Event"],
-    date: "January 28, 2022"
-  },
-  {
-    name: ["Daily Missions Updated","Sweet Shenanigans Story Event","Marnie (Palentine's 2022) & Mawile"],
-    date: "January 31, 2022"
-  },
-  {
-    name: ["Bea (Palentine's 2022) & Vanilluxe"],
-    date: "February 2, 2022"
-  },
-  {
-    name: ["Villain Arc's Sinnoh Chapter Updated", "Cyrus & Palkia's Power Boost / Grid Update"],
-    date: "February 10, 2022"
-  },
-  {
-    name: ["Run-Up to 2.5 Years Rally, Log-in Bonus, and Mission Bingo"],
-    date: "February 17, 2022"
-  },
-  {
-    name: ["Eggs Updated!"],
-    date: "February 24, 2022"
-  },
-  {
-    name: ["EggDex","Champion Stadium Battle Tips & Other Content"],
+    name: ["EggDex","Connecting/Interacting With Trainers"],
     date: "TBA"
   }
 ];
